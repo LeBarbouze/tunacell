e56705afe546c7bdeeb3e98fd586d2927d0e6467
Joachim Rambeau <joachim.rambeau@gmail.com>
2019-08-12 01:35:25 +0200

    Edit scope to session

--- tunacell/tests/conftest.py
+++ tunacell/tests/conftest.py
@@ -12,7 +12,7 @@ path_data = os.path.join(os.path.dirname(tunacell.__file__), 'data')
 path_fake_exp = os.path.join(path_data, 'fake')
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='session')
 def fake_exp():
     exp = Experiment(path_fake_exp, count_items=True)
     yield exp
@@ -20,3 +20,6 @@ def fake_exp():
     analysis_path = os.path.join(path_fake_exp, 'analysis')
     if os.path.exists(analysis_path):
         shutil.rmtree(analysis_path)
+    # remove internals path
+    if exp.path_internals.exists():
+        shutil.rmtree(str(exp.path_internals))
