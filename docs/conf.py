08342d84deb8cbe283dd30e9cda13bfea866c99c
Joachim Rambeau <joachim.rambeau@gmail.com>
2019-09-08 16:52:14 +0200

    Try lighter hack code for git-lfs

--- docs/conf.py
+++ docs/conf.py
@@ -35,38 +35,9 @@ sys.path.insert(0, DOC_SOURCES_DIR)
 # because of that. Am I right?
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
-
-    render_examples = False
-
     # hack for lacking git-lfs support on rtd
     import git_lfs
-    try:
-        from urllib.error import HTTPError
-    except ImportError:
-        from urllib2 import HTTPError
-
-    _fetch_urls = git_lfs.fetch_urls
-
-    def _patched_fetch_urls(lfs_url, oid_list):
-        """Hack git_lfs library that sometimes makes too big requests"""
-        objects = []
-
-        try:
-            objects.extend(_fetch_urls(lfs_url, oid_list))
-        except HTTPError as err:
-            if err.code != 413:
-                raise
-            logger.error("LFS: request entity too large, splitting in half")
-            objects.extend(_patched_fetch_urls(lfs_url, oid_list[:len(oid_list) // 2]))
-            objects.extend(_patched_fetch_urls(lfs_url, oid_list[len(oid_list) // 2:]))
-
-        return objects
-
-    git_lfs.fetch_urls = _patched_fetch_urls
     git_lfs.fetch(PROJECT_ROOT_DIR)
-
-else:
-    render_examples = True
 
 
 # -- General configuration ------------------------------------------------
